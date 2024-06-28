1418a784464c29cc6a9a9337e00ab879df9c0ab0
clementRTB2 <cmcolombat@gmail.com>
2024-06-28 15:29:57 +0200

    push project

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">PLAY</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
+</template>
+
+<script>
+
+
+import Block from './components/Block'
+import Results from './components/Results'
+
+export default {
+  name: 'App',
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: { 
+    start() {
+      this.delay = 1000 + Math.random() * 2000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+  body {
+    background-color: #3c3d3d; 
+    color: white; 
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+.content {
+    padding: 20px;
+    text-align: center;
+}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #ffffff;
+    margin-top: 60px;
+  }
+  button {
+    background: #439b09;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank : null
+    }
+  },
+  mounted() {
+    if (this.score < 200) {
+      this.rank = 'incroyable'
+    } else if (this.score < 400) {
+      this.rank = 'Bon reflexes'
+    } else {
+      this.rank = 'tu es lent'
+    }
+  }
+}
+</script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #439b09;
+    font-weight: bold;
+  }
+</style>
